f74eaf063b9b662dffad6de50e8c92cb6d500bef
Nate Butler <iamnbutler@gmail.com>
2016-01-25 10:11:33 -0500

    Add view switching logic to student index

--- assets/js/routes/students.js
+++ assets/js/routes/students.js
@@ -1,4 +1,19 @@
 
 module.exports = function () {
+	// Grid + List toggle
+	$('#grid-view-target').on( "click", function(){
+		$('.grid-view').addClass('active');
+		$('.list-view').removeClass('active');
+		$('.student-grid').show();
+		$('.student-list').hide();
+		return false;
+	});
 
+	$('#list-view-target').on( "click", function(){
+		$('.list-view').addClass('active');
+		$('.grid-view').removeClass('active');
+		$('.student-list').show();
+		$('.student-grid').hide();
+		return false;
+	});
 }
