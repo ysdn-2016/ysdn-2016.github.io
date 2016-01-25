8a7910bba0709e1eb60d32bf7816fa9437ef3c40
Nate Butler <iamnbutler@gmail.com>
2016-01-25 10:30:39 -0500

    Set up category filtering logic

--- assets/js/routes/students.js
+++ assets/js/routes/students.js
@@ -16,4 +16,16 @@ module.exports = function () {
 		$('.student-grid').hide();
 		return false;
 	});
+
+	// Category filtering logic
+	$('.category-target').on( "click", function(){
+		$('.category-target').removeClass('active');
+		$(this).addClass('active');
+		$('.student').show(); // Reset any filtered rows
+		return false;
+	});
+
+	$('.category-com-target').on( "click", function(){
+		$('.student').not('.communication').hide();
+	});
 }
