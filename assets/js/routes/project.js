60f9f529bacc2455f6004a84fe43087b364f54bb
Taylor Emmerson <emmersonic@me.com>
2016-01-30 14:46:13 -0500

    Aligned Section Nav with section

--- assets/js/routes/project.js
+++ assets/js/routes/project.js
@@ -98,7 +98,7 @@ module.exports = function () {
       });
       $('a').click(function () {
         $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top - 60
+          scrollTop: $($(this).attr('href')).offset().top - 80
         }, 500);
         return false;
       });
