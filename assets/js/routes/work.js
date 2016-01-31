2b2c710b700a0995c7d46b0f9e12ec4e6362e9ca
Ross Zurowski <ross@rosszurowski.com>
2016-01-31 00:35:49 -0500

    Add lazyloading to project index page

--- assets/js/routes/work.js
+++ assets/js/routes/work.js
@@ -1,1 +1,11 @@
-module.exports = function () {};
+
+module.exports = function () {
+	$('img').unveil(200, function () {
+		var $image = $(this);
+		$image.load(function () {
+			setTimeout(function () {
+				$image.attr('lazyloaded', true);
+			}, 10);
+		})
+	});
+};
