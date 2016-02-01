930401decc9ccb04d9a4a48d8492229e216654b4
Taylor Emmerson <emmersonic@me.com>
2016-01-31 21:04:47 -0500

    Move Videos to image column, make them responsive

--- assets/js/routes/project.js
+++ assets/js/routes/project.js
@@ -1,6 +1,37 @@
 module.exports = function () {
   if ($('.project--standard')[0]) {
-    console.log('Running');
+    $(document).ready(function () {
+      $('iframe').prependTo('#gallery .project-assets');
+    });
+
+  // Find all YouTube videos
+  var $allVideos = $("iframe"),
+      // The element that is fluid width
+      $fluidEl = $(".project-assets");
+  console.log($allVideos);
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
+    $(this)
+      .data('aspectRatio', this.height / this.width)
+      // and remove the hard coded width/height
+      .removeAttr('height')
+      .removeAttr('width');
+  });
+  // When the window is resized
+  $(window).resize(function() {
+    var newWidth = $fluidEl.width();
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+      var $el = $(this);
+      $el
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
+    });
+  // Kick off one resize to fix all videos on page load
+  }).resize();
+
+
+    console.log('Project Running');
     var windw = this;
     $.fn.followTo = function () {
       var overlay = $('#project-fixed-overlay'),
