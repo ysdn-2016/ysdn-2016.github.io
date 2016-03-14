9e9a7bd879b94c6f01e2efc9589a7ac997b77d5f
Nate Butler <iamnbutler@gmail.com>
2016-03-14 15:25:32 -0400

    Experiment with Parallax in header

--- assets/js/routes/student.js
+++ assets/js/routes/student.js
@@ -1,1 +1,15 @@
-module.exports = function () {};
+module.exports = function () {
+	$(document).ready(function () {
+    $('.main').mousemove(function (e) {
+        // parallax(e, document.getElementById('student-image'), 0.1);
+        parallax(e, document.getElementById('student-shapes'), 0.4);
+    });
+	});
+
+	function parallax(e, target, layer) {
+	    var layer_coeff = 10 / layer;
+	    var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
+	    var y = (e.pageY - ($(window).height() / 2)) / layer_coeff;
+	    $(target).offset({ top: 80 + y ,left : 240 + x });
+	};
+};
