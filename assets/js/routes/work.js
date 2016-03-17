3052ff68b11674d98c33c2bd88e8c155de915188
Ross Zurowski <ross@rosszurowski.com>
2016-03-17 00:41:21 -0400

    Add dumb shuffling of projects

--- assets/js/routes/work.js
+++ assets/js/routes/work.js
@@ -1,7 +1,13 @@
 
+var shuffle = require('../lib/shuffle');
+
 module.exports = function () {
+
+	var grid = shuffle(document.querySelector('[data-columns]'))
+
 	savvior.init('[data-columns]', {
     'screen and (max-width: 800px)': { columns: 2 },
     'screen and (min-width: 800px)': { columns: 3 },
   });
+
 };
