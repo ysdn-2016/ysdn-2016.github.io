0f7173cc7c0113527edb0ab29cf528508e0b713b
Ross Zurowski <ross@rosszurowski.com>
2016-03-20 17:12:01 -0400

    More event page progress

--- assets/js/routes/event.js
+++ assets/js/routes/event.js
@@ -1,16 +1,19 @@
 
 module.exports = function (ctx) {
-	setTimeout(setActionWork, 50)
+	var $html = $('html');
+	var $panel = $('.event-panel');
+
+	setTimeout(setActionWork, 50);
 };
 
 module.exports.exit = function (ctx) {
-	setTimeout(setActionShow, 50)
+	setTimeout(setActionShow, 50);
 }
 
 function setActionShow () {
-	$('.event-ribbon-info-cta').text('Event Information')
+	$('.event-ribbon-info-cta').text('Event Information');
 }
 
 function setActionWork () {
-	$('.event-ribbon-info-cta').text('Student Work')
+	$('.event-ribbon-info-cta').text('Student Work');
 }
