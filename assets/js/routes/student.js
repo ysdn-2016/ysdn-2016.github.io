93707345122cffcd9465e819574000d10b779c63
Nate Butler <iamnbutler@gmail.com>
2016-03-21 15:33:54 -0400

    Prototype profile header disco

--- assets/js/routes/student.js
+++ assets/js/routes/student.js
@@ -3,15 +3,18 @@ module.exports = function () {
     'screen and (min-width: 1px)': { columns: 2 }
   });
 
-  // $('.main').mousemove(function (e) {
-  //   // parallax(e, document.getElementById('student-image'), 0.1);
-  //   parallax(e, $('.student-shapes').get(0), 0.4);
-  // });
+  $('.student-profile').mousemove(function (e) {
+    parallax(e, $('.shape-1-wrapper'), 0.6);
+    parallax(e, $('.shape-2-wrapper'), 1.2);
+    parallax(e, $('.shape-3-wrapper'), 0.4);
+    parallax(e, $('.shape-4-wrapper'), -0.8);
+    parallax(e, $('.shape-5-wrapper'), -0.4);
+  });
 
-// function parallax (e, target, layer) {
-//   var layer_coeff = 10 / layer;
-//   var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
-//   var y = (e.pageY - ($(window).height() / 2)) / layer_coeff;
-//   $(target).offset({ top: 80 + y, left: 240 + x });
-// }
+function parallax (e, target, layer) {
+  var layer_coeff = 10 / layer;
+  var x = (e.pageX / 3) / layer_coeff;
+  var y = (e.pageY / 2) / layer_coeff;
+  $(target).css("transform", "translateX(" + x + "px) translateY(" + y + "px)");
+}
 };
