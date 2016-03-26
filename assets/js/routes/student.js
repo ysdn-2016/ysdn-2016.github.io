416732023491a9c8d35614e81f1add24c2727f18
Ross Zurowski <ross@rosszurowski.com>
2016-03-25 20:55:25 -0400

    Student profile clean-up

--- assets/js/routes/student.js
+++ assets/js/routes/student.js
@@ -1,15 +1,7 @@
+
 module.exports = function () {
 
-  var config = {
-    container: '[data-columns]',
-    items: '.student-profile-project',
-    columnClass: 'column',
-    mediaQueries: [
-      { query: 'screen and (min-width: 1px)', columns: 2 }
-    ]
-  }
-
-  var grid = new Quartz(config)
+  configureGrid();
 
   $('.student-profile').mousemove(function (e) {
     parallax(e, $('.shape-1-wrapper'), 0.15);
@@ -19,10 +11,27 @@ module.exports = function () {
     parallax(e, $('.shape-5-wrapper'), -0.1);
   });
 
-function parallax (e, target, layer) {
-  var layer_coeff = 10 / layer;
-  var x = (e.pageX / 3) / layer_coeff;
-  var y = (e.pageY / 2) / layer_coeff;
-  $(target).css("transform", "translateX(" + x + "px) translateY(" + y + "px)");
-}
+  function parallax (e, target, layer) {
+    var layer_coeff = 10 / layer;
+    var x = (e.pageX / 3) / layer_coeff;
+    var y = (e.pageY / 2) / layer_coeff;
+    $(target).css("transform", "translateX(" + x + "px) translateY(" + y + "px)");
+  }
 };
+
+
+function configureGrid () {
+
+  if (!document.querySelector('[data-columns]')) return;
+
+  var config = {
+    container: '[data-columns]',
+    items: '[data-columns] .student-profile-project',
+    columnClass: 'column',
+    mediaQueries: [
+      { query: 'screen and (min-width: 1px)', columns: 2 }
+    ]
+  }
+
+  var grid = new Quartz(config)
+}
