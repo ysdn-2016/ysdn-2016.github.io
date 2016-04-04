8d0774f4bca8dffa95bfcfa6f695b326e283a318
Ross Zurowski <ross@rosszurowski.com>
2016-04-04 16:28:05 -0400

    Mobile event ribbon / panel progress

--- assets/js/lib/ribbon.js
+++ assets/js/lib/ribbon.js
@@ -4,6 +4,8 @@ var css = require('veinjs');
 var DEFAULT_URL = '/work/';
 var EVENT_URL = '/event/';
 
+var SCROLL_THRESHOLD = 10;
+
 var noop = function () {};
 
 module.exports = (function () {
@@ -16,6 +18,7 @@ module.exports = (function () {
   var $eventRibbonMenuToggle;
 
   var isOpen = false;
+  var isShowingOnMobile = false;
   var isMobileSize = false;
   // NOTE: we're using user agent because the bug this fixes relates to
   // Safari's browser chrome, not the smaller browser size
@@ -28,8 +31,10 @@ module.exports = (function () {
   var mobileHandler = {
     match: function () {
       isMobileSize = true;
+      isShowingOnMobile = true;
       $window.on('resize', fixMobileTransition)
       fixMobileTransition();
+      $window.on('scrolldelta', handleMobileScroll);
     }
   }
 
@@ -95,6 +100,39 @@ module.exports = (function () {
     isOpen = false;
   }
 
+  function showMobileMenu () {
+    $eventRibbonMenu.addClass('event-ribbon-menu--open');
+    $eventRibbonMenuToggle.addClass('event-ribbon-menu-toggle--open');
+  }
+
+  function hideMobileMenu () {
+    $eventRibbonMenu.removeClass('event-ribbon-menu--open');
+    $eventRibbonMenuToggle.removeClass('event-ribbon-menu-toggle--open');
+  }
+
+  function showMobileRibbon () {
+    $eventRibbon.removeClass('event-ribbon--minimized');
+  }
+
+  function hideMobileRibbon () {
+    $eventRibbon.addClass('event-ribbon--minimized');
+  }
+
+  function fixMobileTransition () {
+    if (!isMobileSize) return;
+    var windowHeight = window.innerHeight;
+    var ribbonOffset = windowHeight - $eventRibbon.innerHeight();
+    css.inject('.event-panel', { height: ribbonOffset + 'px', bottom: -ribbonOffset + 'px' });
+    css.inject('.event-panel--open', { transform: 'translateY(-' + ribbonOffset + 'px)' });
+    css.inject('.event-ribbon--open', { transform: 'translateY(-' + ribbonOffset + 'px)' });
+  }
+
+  function unfixMobileTransition () {
+    css.inject('.event-panel', { height: null, bottom: null });
+    css.inject('.event-panel--open', { transform: null });
+    css.inject('.event-ribbon--open', { transform: null });
+  }
+
   function handleEventRibbonClick (e) {
     e.preventDefault();
     if ($eventPanel.hasClass('event-panel--open')) {
@@ -117,29 +155,14 @@ module.exports = (function () {
     }
   }
 
-  function showMobileMenu () {
-    $eventRibbonMenu.addClass('event-ribbon-menu--open');
-    $eventRibbonMenuToggle.addClass('event-ribbon-menu-toggle--open');
-  }
-
-  function hideMobileMenu () {
-    $eventRibbonMenu.removeClass('event-ribbon-menu--open');
-    $eventRibbonMenuToggle.removeClass('event-ribbon-menu-toggle--open');
-  }
-
-  function fixMobileTransition () {
-    if (!isMobileSize) return;
-    var windowHeight = window.innerHeight;
-    var ribbonOffset = windowHeight - $eventRibbon.innerHeight();
-    css.inject('.event-panel', { height: ribbonOffset + 'px', bottom: -ribbonOffset + 'px' });
-    css.inject('.event-panel--open', { transform: 'translateY(-' + ribbonOffset + 'px)' });
-    css.inject('.event-ribbon--open', { transform: 'translateY(-' + ribbonOffset + 'px)' });
-  }
-
-  function unfixMobileTransition () {
-    css.inject('.event-panel', { height: null, bottom: null });
-    css.inject('.event-panel--open', { transform: null });
-    css.inject('.event-ribbon--open', { transform: null });
+  function handleMobileScroll (e) {
+    var scrollY = e.scrollTop;
+    var delta = e.scrollTopDelta;
+    if (delta < 1 && delta <= (SCROLL_THRESHOLD * -1)) {
+      showMobileRibbon();
+    } else if (delta > 1 && delta >= SCROLL_THRESHOLD && scrollY > 0) {
+      hideMobileRibbon();
+    }
   }
 
   function preventDefault (e) {
--- assets/js/routes/event.js
+++ assets/js/routes/event.js
@@ -1,5 +1,33 @@
 
+var mapOptions = {
+	zoom: 14,
+	backgroundColor: 'none',
+	// center: new google.maps.LatLng(43.631014, -79.426256),
+	scrollwheel: false,
+	disableDefaultUI: true,
+	disableDoubleClickZoom: true,
+	styles: require('../templates/map')
+}
 
 module.exports = function (ctx) {
 	var $eventPanel = $('.event-panel');
+	var $eventMap = $('.event-directions-map');
+
+	// var map = new google.maps.Map($eventMap.get(0), mapOptions);
+	// var marker = new google.maps.Marker({
+	// 	position: new google.maps.LatLng(43.631014, -79.426256),
+	// 	map: map,
+	// 	icon: '/assets/images/marker.svg',
+	// 	title: 'Liberty Grand'
+	// })
+
+	// google.maps.event.addDomListener(marker, 'click', onMapMarkerClick)
+
+	/**
+	 * Private functions
+	 */
+
+	function onMapMarkerClick () {
+		window.open('https://goo.gl/maps/YuXwP9XNDhQ2', '_blank').focus()
+	}
 };
